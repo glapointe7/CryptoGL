47271df3088b32721e2466b8b5724479e6d36962
Gabriel <gabriel@gabriel-PC.(none)>
2013-03-25 11:56:52 -0400

    Add StringCipher.cpp and StringCipher.h

---
+++ StringCipher.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   StringCipher.cpp
+ * Author: gabriel
+ * 
+ * Created on March 23, 2013, 9:12 AM
+ */
+
+#include "StringCipher.h"
+
+StringCipher::StringCipher() 
+{
+    
+}
+
+StringCipher::StringCipher(const StringCipher& orig) {
+}
+
---
+++ StringCipher.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   StringCipher.h
+ * Author: gabriel
+ *
+ * Created on March 23, 2013, 9:12 AM
+ */
+
+#ifndef STRINGCIPHER_H
+#define	STRINGCIPHER_H
+
+#include <string>
+
+using namespace std;
+
+class StringCipher 
+{
+    public:
+        StringCipher();
+        StringCipher(const StringCipher& orig);
+        virtual ~StringCipher() = default;
+
+        virtual string encode()const = 0;
+        virtual string decode()const = 0;
+        //virtual void setKey(const string key);
+
+    protected:
+       string cipher_text;
+       string clear_text;
+       string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //Data DataFile;
+};
+
+#endif	/* STRINGCIPHER_H */
+
