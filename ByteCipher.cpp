abda692774af8953d921987cdb53dbc3a25f1fd8
Gabriel <gabriel@gabriel-PC.(none)>
2013-03-27 18:22:42 -0400

    Ajout des fichier h et cpp de ByteCipher.

---
+++ ByteCipher.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   ByteCipher.cpp
+ * Author: gabriel
+ * 
+ * Created on March 26, 2013, 4:56 PM
+ */
+
+#include "ByteCipher.h"
+
+ByteCipher::ByteCipher()
+{
+}
+
+ByteCipher::ByteCipher(const ByteCipher& orig)
+{
+}
+
+ByteCipher::~ByteCipher()
+{
+}
---
+++ ByteCipher.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   ByteCipher.h
+ * Author: gabriel
+ *
+ * Created on March 26, 2013, 4:56 PM
+ */
+
+#ifndef BYTECIPHER_H
+#define	BYTECIPHER_H
+
+class ByteCipher
+{
+public:
+   ByteCipher();
+   ByteCipher(const ByteCipher& orig);
+   virtual ~ByteCipher();
+
+
+
+protected:
+
+};
+
+#endif	/* BYTECIPHER_H */
+
