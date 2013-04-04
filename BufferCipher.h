a9cded7c8b11f2abd70378162a325cb53c156f42
Gabriel <gabriel@gabriel-PC.(none)>
2013-04-04 12:11:44 -0400

    Ajout de méthodes.

--- BufferCipher.h
+++ BufferCipher.h
@@ -2,18 +2,30 @@
 #ifndef BUFFERCIPHER_H
 #define	BUFFERCIPHER_H
 
+#include "Cipher.hpp"
 
+#include <vector>
+#include <string>
 
-class BufferCipher
+typedef std::vector<unsigned char> Bytes_Container;
+
+class BufferCipher : public Cipher<Bytes_Container>
 {
 public:
    BufferCipher();
    virtual ~BufferCipher() {}
 
+   void save(const std::string &, const Bytes_Container &);
+   Bytes_Container load(const std::string &);
 
+   // Pour obtenir les octets sous forme d'une chaîne hexadécimale.
+   std::string hexDigest(const Bytes_Container &data) const;
 
 protected:
-
+   
+private:
+   virtual Bytes_Container encode(const Bytes_Container &) = 0;
+   virtual Bytes_Container decode(const Bytes_Container &) = 0;
 };
 
 #endif
