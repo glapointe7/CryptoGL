2bebed68be57526cc5a7a8d4316a0066761ec3fd
Gabriel <gabriel@gabriel-PC.(none)>
2013-04-09 16:06:48 -0400

    Factory Design avec transposition en ligne, colonne et double.

--- Transposition.cpp
+++ Transposition.cpp
@@ -7,7 +7,7 @@
 
 Transposition::Transposition()
 {
-   
+
 }
 
 void Transposition::setKey(const std::string &key)
@@ -15,45 +15,107 @@ void Transposition::setKey(const std::string &key)
    this->key = key;
 }
 
-std::vector<std::string> Transposition::setTable(const std::string &data) const
+void Transposition::setStartingTable(const std::string &data)
 {
-   std::vector<std::string> table;
    unsigned int key_len = key.length();
-   
+
+   std::string full_data(data);
+   full_data.append(key_len - (data.length() % key_len), 'X');
+   unsigned int data_len = full_data.length();
+
    // On remplit la grille qu'on doit lire en colonne. 
-   // Pour faciliter le code, on la transpose.
-   unsigned int data_len = data.length();
-   std::string tmp;
-   tmp.reserve(data_len);
-   for (unsigned int i = 0; i < key_len; i++)
+   for (unsigned int i = 0; i < data_len; i += key_len)
    {
-      tmp = "";
-      for (unsigned int k = i; k < data_len; k += key_len)
-      {
-         tmp += data[k];
-      }
-      table.push_back(tmp);
+      table.push_back(full_data.substr(i, key_len));
    }
-   
-   return table;
 }
 
-// Ordonner les lignes selon l'ordre alphabétique des lettres de la clé.
-std::vector<std::string> Transposition::sortColon(const std::vector<std::string> &table) const
+std::string Transposition::readFinalTable(const std::vector<std::string>& s_table) const
 {
-   unsigned int key_len = key.length();
+   std::string data = "";
+   data.reserve(s_table.size() * key.length());
+   for (auto str : s_table)
+   {
+      data += str;
+   }
+
+   return data;
+}
+
+// Trie la clé en ordre alphabétique en conservant les positions originales de chaque lettre.
+
+std::map<char, unsigned int> Transposition::sortKey() const
+{
    std::map<char, unsigned int> sorted_key;
-   
-   for(unsigned int i = 0; i < key_len; i++)
+   unsigned int key_len = key.length();
+
+   for (unsigned int i = 0; i < key_len; i++)
    {
       sorted_key.insert(std::make_pair(key[i], i));
    }
-   
+
+   return sorted_key;
+}
+
+// Ordonner les lignes selon l'ordre alphabétique des lettres de la clé.
+
+std::vector<std::string>
+Transposition::swapColumnsDecode(const std::map<char, unsigned int>& sorted_key) const
+{
    std::vector<std::string> sorted_table;
-   for(auto pair : sorted_key)
+   unsigned int key_len = key.length();
+   unsigned int rows = table.size();
+
+   for (unsigned int i = 0; i < rows; i++)
    {
-      sorted_table.push_back(table[pair.second]);
+      std::string row(key_len, 'A');
+      unsigned int j = 0;
+      for (auto pair : sorted_key)
+      {
+         row[key.find(pair.first)] = table[i][j];
+         j++;
+      }
+      sorted_table.push_back(row);
    }
-   
+
    return sorted_table;
+}
+
+std::vector<std::string>
+Transposition::swapColumnsEncode(const std::map<char, unsigned int>& sorted_key) const
+{
+   std::vector<std::string> sorted_table;
+   unsigned int key_len = key.length();
+   unsigned int rows = table.size();
+
+   for (unsigned int i = 0; i < rows; i++)
+   {
+      std::string row;
+      row.reserve(key_len);
+      for (auto pair : sorted_key)
+      {
+         row += table[i][pair.second];
+      }
+      sorted_table.push_back(row);
+   }
+
+   return sorted_table;
+}
+
+std::string Transposition::getEncoded(const std::string& data, const Transposition_types type)
+{
+   setStartingTable(data);
+   const std::vector<std::string> s_table = swapColumnsEncode(sortKey());
+
+   Transposition *T = TranspositionFactory::createTransposition(type);
+   return T->read(s_table);
+}
+
+std::string Transposition::getDecoded(const std::string& data, const Transposition_types type)
+{
+   Transposition *T = TranspositionFactory::createTransposition(type);
+   T->setTable(data);
+   const std::vector<std::string> s_table = swapColumnsDecode(sortKey());
+
+   return readFinalTable(s_table);
 }
--- Transposition.hpp
+++ Transposition.hpp
@@ -5,24 +5,201 @@
 #include "StringCipher.h"
 #include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
 
 class Transposition : public StringCipher
 {
 public:
    Transposition();
-   virtual ~Transposition() {}
-   
+
+   virtual ~Transposition()
+   {
+   }
+
+   enum class Transposition_types : uint8_t
+   {
+      Columns = 0,
+              Rows = 1,
+              Double = 2
+   };
+
    virtual std::string encode(const std::string &) = 0;
    virtual std::string decode(const std::string &) = 0;
-   
+
    void setKey(const std::string &key);
-   
-private:
-   std::vector<std::string> setTable(const std::string &data) const;
-   std::vector<std::string> sortColon(const std::vector<std::string> &table) const;
-   
+
+protected:
+   virtual void setTable(const std::string &data) = 0;
+   virtual std::string read(const std::vector<std::string>&) const = 0;
+
+   void setStartingTable(const std::string &data);
+   std::string readFinalTable(const std::vector<std::string>&) const;
+
+   std::map<char, unsigned int> sortKey() const;
+   std::vector<std::string> swapColumnsEncode(const std::map<char, unsigned int>& sorted_key) const;
+   std::vector<std::string> swapColumnsDecode(const std::map<char, unsigned int>& sorted_key) const;
+
+   std::string getEncoded(const std::string& data, const Transposition_types type);
+   std::string getDecoded(const std::string& data, const Transposition_types type);
+
    std::string key;
+   std::vector<std::string> table;
+};
+
+// Transposition en ligne : permutation des colonnes du tableau et lecture en colonne.
+class TranspositionColumns : public Transposition
+{
+public:
+
+   std::string encode(const std::string &clear_text)
+   {
+      return getEncoded(clear_text, Transposition_types::Columns);
+   }
+
+   std::string decode(const std::string &cipher_text)
+   {
+      return getDecoded(cipher_text, Transposition_types::Columns);
+   }
+
+   // Lecture en colonne du tableau.
+
+   std::string read(const std::vector<std::string> &s_table) const
+   {
+      unsigned int key_len = key.length();
+      std::string data = "";
+      data.reserve(s_table.size() * key_len);
+      for (unsigned int i = 0; i < key_len; i++)
+      {
+         for (auto str : s_table)
+         {
+            data += str[i];
+         }
+      }
+
+      return data;
+   }
+
+   // Remplit le tableau avec data selon la longueur de la clé représentant le nombre de colonnes.
+
+   void setTable(const std::string &data)
+   {
+      unsigned int key_len = key.length();
+      unsigned int data_len = data.length();
+      unsigned int table_size = data_len / key_len;
+
+      for (unsigned int i = 0; i < key_len; i++)
+      {
+         std::string row;
+         row.reserve(key_len);
+         for (unsigned int j = 0; j < data_len; j += table_size)
+         {
+            row += data[j];
+         }
+         table.push_back(row);
+      }
+   }
+
+};
+
+// Transposition en ligne : permutation des colonnes du tableau et lecture en ligne.
+
+class TranspositionRows : public Transposition
+{
+public:
+
+   std::string encode(const std::string &clear_text)
+   {
+      return getEncoded(clear_text, Transposition_types::Rows);
+   }
+
+   std::string decode(const std::string &cipher_text)
+   {
+      return getDecoded(cipher_text, Transposition_types::Rows);
+   }
+
+   std::string read(const std::vector<std::string> &s_table) const
+   {
+      return readFinalTable(s_table);
+   }
+
+   void setTable(const std::string &data)
+   {
+      setStartingTable(data);
+   }
+};
+
+// Chiffre à transposition double : permutation des colonnes puis des lignes du tableau.
+
+class TranspositionDouble : public Transposition
+{
+public:
+
+   std::string encode(const std::string &clear_text)
+   {
+      return getEncoded(clear_text, Transposition_types::Double);
+   }
+
+   std::string decode(const std::string &cipher_text)
+   {
+      return getDecoded(cipher_text, Transposition_types::Double);
+   }
+
+   std::string read(const std::vector<std::string> &s_table) const
+   {
+      std::vector<std::string> col_table;
+      for (auto row : key_row)
+      {
+         col_table.push_back(s_table[row]);
+      }
+
+      return readFinalTable(col_table);
+   }
+
+   void setTable(const std::string &data)
+   {
+      setStartingTable(data);
+      unsigned int key_len = key_row.size();
+      std::vector<std::string> tmp;
+
+      for (unsigned int i = 0; i < key_len; i++)
+      {
+         auto pos = std::find(key_row.begin(), key_row.end(), i);
+         tmp.push_back(table[*pos]);
+      }
+      table = tmp;
+   }
+
+   void setKeyRow(std::vector<unsigned int>& key_row)
+   {
+      this->key_row = key_row;
+   }
+
+private:
+   std::vector<unsigned int> key_row;
+};
+
+class TranspositionFactory
+{
+public:
+
+   virtual ~TranspositionFactory()
+   {
+   }
+
+   static Transposition* createTransposition(Transposition::Transposition_types T_type)
+   {
+      switch (T_type)
+      {
+         case Transposition::Transposition_types::Columns:
+            return new TranspositionColumns();
+         case Transposition::Transposition_types::Rows:
+            return new TranspositionRows();
+         case Transposition::Transposition_types::Double:
+            return new TranspositionDouble();
+      }
+      throw "Transposition choice is inexistent.";
+   }
 };
 
 #endif	/* TRANSPOSITION_HPP */
-
