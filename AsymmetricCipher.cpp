ddc67db5d44dc1f616b772cf1d5196e0bfcecea5
Gabriel <gabriel@gabriel-PC.(none)>
2013-04-10 21:09:17 -0400

    Ajout d'une famille de crypto : AsymmetricCipher.

---
+++ AsymmetricCipher.cpp
@@ -0,0 +1,2 @@
+#include "AsymmetricCipher.hpp"
+
---
+++ AsymmetricCipher.hpp
@@ -0,0 +1,32 @@
+#ifndef ASYMMETRICCIPHER_HPP
+#define	ASYMMETRICCIPHER_HPP
+
+#include "Cipher.hpp"
+
+#include <vector>
+#include <string>
+
+class AsymmetricCipher : public Cipher<std::vector<unsigned long>, std::vector<unsigned char> >
+{
+public:
+   
+   typedef std::vector<unsigned long> Numbers;
+   typedef std::vector<unsigned char> Bytes_Container;
+   
+   virtual ~AsymmetricCipher();
+   
+   virtual Numbers encode(const Bytes_Container &) = 0;
+   virtual Bytes_Container decode(const Numbers &) = 0;
+   
+   static void save(const std::string &, const std::string &);
+   static std::string load(const std::string &);
+   
+   // Pour obtenir les octets sous forme d'une chaîne hexadécimale.
+   std::string hexDigest(const Bytes_Container &) const;
+   std::string toString(const Numbers &) const;
+   
+private:
+
+};
+
+#endif	/* ASYMMETRICCIPHER_HPP */
